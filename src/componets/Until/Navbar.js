2ebb22c2d36145c6352b5736954cf01aa04a92f2
Bruno <bruno.umbelino0@gmail.com>
2021-03-05 03:40:20 -0300

    implementing responsive Navbar

--- src/componets/Until/Navbar.js
+++ src/componets/Until/Navbar.js
@@ -1,54 +1,97 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { PrimaryButton } from "./Button";
 import logo from "../../icon.png";
+import {
+  Collapse,
+  Nav,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarText,
+} from "reactstrap";
 
-function Navbar() {
+function MyNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <NavWrapper className="navbar navbar-expand-sm px-sm-4">
-      <Link to="/">
-        <img src={logo} alt="phone-store" className="navbar-brand" />
-      </Link>
-      <ul className="navbar-nav align-items-center ml-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Products
-          </Link>
-        </li>
-      </ul>
-      <Link to="/cart" className="ml-2">
-        <PrimaryButton>
-          <span className="mr-2">
-            <i className="fas fa-cart-plus" />
-          </span>
-          My cart
-        </PrimaryButton>
-      </Link>
+    <NavWrapper>
+      <Navbar
+        color="blue"
+        dark
+        expand="md"
+        className="align-content-center my-0 py-0"
+      >
+        <NavbarBrand href="/">
+          <img src={logo} alt="phone-store" />
+        </NavbarBrand>
+        <NavbarText>Smart Store</NavbarText>
+
+        <NavbarToggler onClick={toggle} />
+
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto align-items-center" navbar>
+            <NavItem>
+              <NavLink href="/">
+                <span className="navlink-product">Products</span>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/cart">
+                <PrimaryButton>
+                  <span className="mr-2">
+                    <i className="fas fa-cart-plus" />
+                  </span>
+                  My cart
+                </PrimaryButton>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </NavWrapper>
   );
 }
 
 const NavWrapper = styled.nav`
-  background-color: var(--red);
-  height: 80px;
-
-  .navbar-brand {
-    width: 50px;
-    background: linear-gradient(120deg, var(--green), var(--blue));
-    border-radius: 8px;
+  .navbar {
+    background-color: var(--red);
+    height: ;
   }
 
-  .nav-link {
-    font-size: 1.6rem;
+  .navbar-dark .navbar-toggler {
+    border: 2px solid white;
+    background-color: #bd200f;
+    border-radius: 5px;
+  }
+
+  .navbar-text {
+    color: white;
+    font-size: 2rem;
+  }
+
+  .navbar-brand img {
+    height: 60px;
+    background: linear-gradient(120deg, var(--green), var(--blue));
+    border-radius: 8px;
+    padding: 5px 0px;
+  }
+
+  .navlink-product {
+    font-size: 1.4rem;
     font-weight: 400;
     color: white;
     text-transform: capitalize;
     transition: all 0.3s ease-in-out;
-  }
-  .nav-link:hover {
-    color: lightgray;
+
+    &:hover {
+      color: black;
+    }
   }
 `;
 
-export default Navbar;
+export default MyNavbar;
